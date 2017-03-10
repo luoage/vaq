b073364c4fc98aef123905940cd0bbdc734a729e
luoage <luoage@msn.cn>
2017-03-10 16:32:49 +0800

    add webpack to test if it supports

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 
 module.exports = {
-  entry: './example/webpack/test.js',
-  output: {
-    filename: 'test.bundle.js',
-    path: path.resolve(__dirname, './example/webpack/')
-  }
+	entry: './example/webpack/test.js',
+	output: {
+		filename: 'test.bundle.js',
+		path: path.resolve(__dirname, './example/webpack/')
+	}
 };
